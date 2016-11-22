b2d79a43aa2f9bb5a1db76f15607cbfad41311d8
Kaci Sofiane <kaci@a12p18.fil.univ-lille1.fr>
2016-11-22 10:38:09 +0100

    supprimer les $..

--- src/controller/TweetsTableController.java
+++ src/controller/TweetsTableController.java
@@ -15,6 +15,7 @@ import model.TweetsTableModel;
 import tools.classification.AbstractClassification;
 import tools.textCleaner.ReplaceStringCleanMethod;
 import tools.textCleaner.TextCleaner;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.AbstractStringMetric;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
@@ -55,13 +56,13 @@ public class TweetsTableController {
 		tweetCleaner.add(new ReplaceStringCleanMethod(" $|^ ", ""));
 		// supression des doubles espace et espaces insécables
 		tweetCleaner.add(new ReplaceStringCleanMethod("  |\u00A0", " "));
-		//supression des sommes avec $ 
-		tweetCleaner.add(new ReplaceStringCleanMethod("$[0-9]*|[0-9]*.[0-9]*", "$XX"));
-		//supression des sommes avec €
-		tweetCleaner.add(new ReplaceStringCleanMethod("€[0-9]*|€[0-9]*.[0-9]*", "€XX"));
+		//supression des sommes avec $ et €
+		tweetCleaner.add(new ReplaceStringCleanMethod("[-#@\n()0-9+&@/%?=~_!:,\\.;\"*><^…]|RT", ""));
 
-		
 	}
+	
+	
+	
 	
 	public void loadToBase(){
 		for(Tweet tweet : tweetsTableModel){
