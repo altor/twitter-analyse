0c60ace256e3d18de80df6325ddaf65aa12006fe
Kaci Sofiane <kaci@a12p15.fil.univ-lille1.fr>
2016-12-06 10:51:12 +0100

    les taux( positifs, neutres,negatifs)

--- src/model/TweetTableClassifier.java
+++ src/model/TweetTableClassifier.java
@@ -1,23 +1,79 @@
 package model;
 
+import twitter.Tweet;
+
 public class TweetTableClassifier extends TweetsTableModel {
 	public TweetTableClassifier(){
 		super();
 	}
 
+	 //Taux des tweet positif
+	
 	public double getPositivePercent(){
 		
-		return 0;
+		double nbPositif =0;
+		double tauxPositif=0;
+		
+		for(Tweet tweet : tweetList){
+			
+		int annotation = tweet.getAnnotation();
+		
+			if(annotation==4){
+			   nbPositif++;
+			
+			}
+		}
+		double nb_total= tweetList.size();
+		tauxPositif=nbPositif/nb_total;
+	
+		return tauxPositif;
 	}
+	
+	//Taux des tweets negatifs
+
 	
 	public double getNegativePercent(){
+		
+		double nbnegatif =0;
+		double taux_negatif=0;
+		
+		for(Tweet tweet : tweetList){
 			
-		return 0;
+		int annotation = tweet.getAnnotation();
+		
+			if(annotation==0){
+			   nbnegatif++;
+			
+			}
+		}
+		
+		double nb_total= tweetList.size();
+		taux_negatif=nbnegatif/nb_total;
+	
+		return taux_negatif;
+		
 	}
 	
+	//Taux des tweet neutres
+
 	public double getNeutralPercent(){
 		
-		return 0;
+		double nb_neutre =0;
+		double taux_neutre=0;
+		
+		for(Tweet tweet : tweetList){
+		int annotation = tweet.getAnnotation();
+		
+			if(annotation==2){
+			   nb_neutre++;
+			
+			}
+		}
+		double nb_total= tweetList.size();
+		taux_neutre=nb_neutre/nb_total;
+	
+		return taux_neutre;
+	
 	}
 	
 	public void classify(){
