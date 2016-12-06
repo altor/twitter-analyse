3ace887547925e3f5fe1da02ad557a0b0bd704b3
Kaci Sofiane <kaci@a12p15.fil.univ-lille1.fr>
2016-12-06 11:35:50 +0100

    bouton recherche classification

---
+++ src/view/action/SearchActionclassifier.java
@@ -0,0 +1,45 @@
+package view.action;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.util.List;
+
+import javax.swing.AbstractAction;
+import javax.swing.Icon;
+import javax.swing.JTextField;
+
+import twitter.Tweet;
+import twitter.TwitterAPI;
+import controller.TweetsTableController;
+
+
+
+public class SearchActionclassifier extends AbstractAction {
+
+	private JTextField requestField;
+	private TwitterAPI twitterAPI;
+	private TweetsTableController tweetsTableController;
+
+	public SearchActionclassifier(String texte, JTextField requestField, TwitterAPI twitterAPI,
+			TweetsTableController tweetsTableController) {
+		super(texte);
+		this.requestField = requestField;
+		this.twitterAPI = twitterAPI;
+		this.tweetsTableController = tweetsTableController;
+		
+	}
+
+	
+
+	public void actionPerformed(ActionEvent e) {
+		try {
+			List<Tweet> tweetList = this.twitterAPI.getTweets(this.requestField.getText());
+			//.updateTableModel(tweetList);
+			tweetsTableController.updateTableClassifier(tweetList);
+
+		} catch (Exception e1) {
+			System.out.println(e1.getMessage());
+		}
+	}
+}
